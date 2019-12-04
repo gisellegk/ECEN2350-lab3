95b3a5661e5fbc4cbdb73fa77c08d6af21aa4e9e
unknown <gisellegk@gmail.com>
2019-12-04 00:27:06 -0700

    skeleton for blinker

--- verilog/blinker.v
+++ verilog/blinker.v
@@ -1,3 +1,17 @@
-module blinker(input clock, input state, output [2:0]LEDs);
+module blinker(input clock, input state, output reg [2:0] left, output reg [2:0] right);
+
+`include "verilog/params.vh" // quartus
+//`include "../verilog/params.vh" // TEST ONLY
+
+always @(clock) begin
+    if(state == TURN) begin
+    // do TURN logic
+    end else if(state == HAZARD) begin
+    // do HAZARD logic
+    end
+    else begin
+    // off
+    end
+end
 
 endmodule
