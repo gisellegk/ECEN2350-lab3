b8a358cb71f1a8e86c91ebbb7953cd592a0cc148
Braedon Bellamy <braedonb98@gmail.com>
2019-12-07 16:57:16 -0700

    trying to fix the blinker

--- verilog/blinker.v
+++ verilog/blinker.v
@@ -10,29 +10,39 @@ counter = 3'b0;
 end
 
 always @(posedge clock) begin
-	left = 0;
-	right = 0; 
-    if(state == TURN) begin
+	left = 3'b000;
+	right = 3'b000; 
+	 
+	 if(state == 0) begin 
+	 //if(state == TURN) begin 
     // do TURN logic
 		if (direction == LEFT) begin
-			if(counter == 0) left[0] = 1;
+			if(counter == 0) left[2] = 1;
 			if (counter == 1) left [1] = 1;
-			if (counter == 2) left [2] = 1;
-			
-		end
+			if (counter == 2) left [0] = 1;
+			end
 		else if (direction == RIGHT) begin
 			if(counter == 0) right[0] = 1;
 			if (counter == 1) right [1] = 1;
 			if (counter == 2) right [2] = 1;
+			//right = 3'b111;
 		end
-		
-    end else if(state == HAZARD) begin
+    end
+	 
+	if(state == HAZARD) begin
     // do HAZARD logic
 		if(counter>1) begin
 		right = 3'b111;
 		left = 3'b111;
 		end
     end
+	 
+	 //if (state == IDLE)begin
+		//right = 3'b000;
+		//left = 3'b000;
+	 //end
+
+	 
 	 
 	 counter = counter +1;
 	 if (counter== 4) counter = 0;
